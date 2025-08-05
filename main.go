dc6490dbff7f1fd787c8f77a92f170bc87fe7ea2
ElrohirGT <elrohirgt@gmail.com>
2025-08-05 10:00:15 -0600

    feat: Added basic usage

--- main.go
+++ main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 
-	p "github.com/ElrohirGT/parser"
+	p "github.com/ElrohirGT/5318008Lang/parser"
+	"github.com/antlr4-go/antlr/v4"
 )
 
 type Listener struct{}
 
 func (Listener) EnterProgram(c *p.ProgramContext) {
+	fmt.Println("Got Text!", c.GetText())
 }
 
 func main() {
-	fmt.Println("Hello World!")
+	inputStream, err := antlr.NewFileStream(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	lexer := p.NewCompiscriptLexer(inputStream)
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+	parser := p.NewCompiscriptParser(stream)
+
+	parser.Program()
 }
