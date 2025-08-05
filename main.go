4aa11cff638f341b4ca21ccf2b17a84b6e74ec9c
ElrohirGT <elrohirgt@gmail.com>
2025-08-05 09:39:24 -0600

    fix: Changed antlr to correct dir

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	p "github.com/ElrohirGT/parser"
+)
+
+type Listener struct{}
+
+func (Listener) EnterProgram(c *p.ProgramContext) {
+}
 
 func main() {
 	fmt.Println("Hello World!")
