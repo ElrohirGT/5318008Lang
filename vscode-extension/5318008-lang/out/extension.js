cf019cb12455915a8a4bb9d7fbc2f137c137a70c
Jose-Prince <joseprince148@gmail.com>
2025-08-29 23:23:08 -0600

    Feat: show errors in real time

--- vscode-extension/5318008-lang/out/extension.js
+++ vscode-extension/5318008-lang/out/extension.js
@@ -5,19 +5,42 @@ exports.deactivate = deactivate;
 const vscode = require("vscode");
 const child_process_1 = require("child_process");
 const path = require("path");
+const fs = require("fs");
+const os = require("os");
+let timeout;
 function activate(context) {
+    var _a;
     console.log("¡Compiscript extension activated!");
     const diagnostic = vscode.languages.createDiagnosticCollection("compiscript");
     context.subscriptions.push(diagnostic);
-    vscode.workspace.onDidSaveTextDocument((document) => {
-        if (document.languageId == "compiscript") {
+    const activeDoc = (_a = vscode.window.activeTextEditor) === null || _a === void 0 ? void 0 : _a.document;
+    if (activeDoc && activeDoc.languageId === "compiscript") {
+        runSemanticAnalyzer(activeDoc, diagnostic);
+    }
+    vscode.workspace.onDidOpenTextDocument((document) => {
+        if (document.languageId === "compiscript") {
             runSemanticAnalyzer(document, diagnostic);
         }
     });
+    vscode.workspace.onDidChangeTextDocument((event) => {
+        if (event.document.languageId === "compiscript") {
+            runWithDebounce(event.document, diagnostic);
+        }
+    });
+}
+function runWithDebounce(document, diagnostics) {
+    if (timeout) {
+        clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+        runSemanticAnalyzer(document, diagnostics);
+    }, 500);
 }
 function runSemanticAnalyzer(document, diagnostics) {
     const analyzePath = path.join(__dirname, "..", "bin", "compiscript-analyzer");
-    (0, child_process_1.execFile)(analyzePath, [document.fileName], (error, stdout, stderr) => {
+    const tmpFile = path.join(os.tmpdir(), `compiscript_${Date.now()}.cps`);
+    fs.writeFileSync(tmpFile, document.getText(), "utf8");
+    (0, child_process_1.execFile)(analyzePath, [tmpFile], (error, stdout, stderr) => {
         const diags = [];
         const output = stdout + "\n" + stderr;
         const lines = output.split("\n");
@@ -27,23 +50,32 @@ function runSemanticAnalyzer(document, diagnostics) {
             if (semanticMatch) {
                 const lineNum = parseInt(semanticMatch[1], 10) - 1;
                 const message = semanticMatch[2];
-                const textLine = document.lineAt(lineNum);
-                const range = new vscode.Range(lineNum, 0, lineNum, textLine.text.length);
-                diags.push(new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Error));
+                if (lineNum >= 0 && lineNum < document.lineCount) {
+                    const textLine = document.lineAt(lineNum);
+                    const range = new vscode.Range(lineNum, 0, lineNum, textLine.text.length);
+                    diags.push(new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Error));
+                }
             }
             else if (syntaxMatch) {
                 const lineNum = parseInt(syntaxMatch[1], 10) - 1;
                 const colNum = parseInt(syntaxMatch[2], 10) - 1;
                 const message = syntaxMatch[3];
-                const textLine = document.lineAt(lineNum);
-                const range = new vscode.Range(lineNum, Math.max(colNum, 0), lineNum, Math.min(colNum + 1, textLine.text.length));
-                diags.push(new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Error));
+                if (lineNum >= 0 && lineNum < document.lineCount) {
+                    const textLine = document.lineAt(lineNum);
+                    const range = new vscode.Range(lineNum, Math.max(colNum, 0), lineNum, Math.min(colNum + 1, textLine.text.length));
+                    diags.push(new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Error));
+                }
             }
         }
         diagnostics.set(document.uri, diags);
         if (error) {
             console.error("Error executing the analyzer:", error);
         }
+        fs.unlink(tmpFile, (err) => {
+            if (err) {
+                console.warn("No se pudo borrar el archivo temporal:", tmpFile);
+            }
+        });
     });
 }
 function deactivate() { }
--- vscode-extension/5318008-lang/src/extension.ts
+++ vscode-extension/5318008-lang/src/extension.ts
@@ -1,17 +1,41 @@
 import * as vscode from "vscode";
 import { execFile } from "child_process";
 import * as path from "path";
+import * as fs from "fs";
+import * as os from "os";
 
-export function activate(context: vscode.ExtensionContext) {    
+let timeout : NodeJS.Timeout | undefined;
+
+export function activate(context: vscode.ExtensionContext) {
     console.log("¡Compiscript extension activated!");
     const diagnostic = vscode.languages.createDiagnosticCollection("compiscript");
     context.subscriptions.push(diagnostic);
 
-    vscode.workspace.onDidSaveTextDocument((document : vscode.TextDocument) => {
-        if (document.languageId == "compiscript") {
+    const activeDoc = vscode.window.activeTextEditor?.document;
+    if (activeDoc && activeDoc.languageId === "compiscript") {
+        runSemanticAnalyzer(activeDoc, diagnostic);
+    }
+
+    vscode.workspace.onDidOpenTextDocument((document: vscode.TextDocument) => {
+        if (document.languageId === "compiscript") {
             runSemanticAnalyzer(document, diagnostic);
         }
-    })
+    });
+
+    vscode.workspace.onDidChangeTextDocument((event: vscode.TextDocumentChangeEvent) => {
+        if (event.document.languageId === "compiscript") {
+            runWithDebounce(event.document, diagnostic);
+        }
+    });
+}
+
+function runWithDebounce(document: vscode.TextDocument, diagnostics: vscode.DiagnosticCollection) {
+    if (timeout) {
+        clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+        runSemanticAnalyzer(document, diagnostics);
+    }, 500);
 }
 
 function runSemanticAnalyzer(
@@ -20,43 +44,40 @@ function runSemanticAnalyzer(
 ) {
     const analyzePath = path.join(__dirname, "..", "bin", "compiscript-analyzer");
 
-    execFile(analyzePath, [document.fileName], (error, stdout, stderr) => {
-        const diags: vscode.Diagnostic[] = [];
+    const tmpFile = path.join(os.tmpdir(), `compiscript_${Date.now()}.cps`);
+    fs.writeFileSync(tmpFile, document.getText(), "utf8");
 
+    execFile(analyzePath, [tmpFile], (error, stdout, stderr) => {
+        const diags: vscode.Diagnostic[] = [];
         const output = stdout + "\n" + stderr;
         const lines = output.split("\n");
 
         for (const line of lines) {
             const semanticMatch = line.match(/^\s*\*?\s*Error: \(line: (\d+)\)\s+(.+)/);
-            const syntaxMatch = line.match(/^\s*line (\d+):(\d+)\s+(.+)$/); 
+            const syntaxMatch = line.match(/^\s*line (\d+):(\d+)\s+(.+)$/);
 
             if (semanticMatch) {
                 const lineNum = parseInt(semanticMatch[1], 10) - 1;
-                const message = semanticMatch[2];                
-
-                const textLine = document.lineAt(lineNum);
-                const range = new vscode.Range(lineNum, 0, lineNum, textLine.text.length);
-                
-                diags.push(
-                    new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Error)
-                );
+                const message = semanticMatch[2];
+                if (lineNum >= 0 && lineNum < document.lineCount) {
+                    const textLine = document.lineAt(lineNum);
+                    const range = new vscode.Range(lineNum, 0, lineNum, textLine.text.length);
+                    diags.push(new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Error));
+                }
             } else if (syntaxMatch) {
                 const lineNum = parseInt(syntaxMatch[1], 10) - 1;
                 const colNum = parseInt(syntaxMatch[2], 10) - 1;
                 const message = syntaxMatch[3];
-
-                const textLine = document.lineAt(lineNum);
-
-                const range = new vscode.Range(
-                    lineNum,
-                    Math.max(colNum, 0),
-                    lineNum,
-                    Math.min(colNum + 1, textLine.text.length)
-                );
-
-                diags.push(
-                    new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Error)
-                );
+                if (lineNum >= 0 && lineNum < document.lineCount) {
+                    const textLine = document.lineAt(lineNum);
+                    const range = new vscode.Range(
+                        lineNum,
+                        Math.max(colNum, 0),
+                        lineNum,
+                        Math.min(colNum + 1, textLine.text.length)
+                    );
+                    diags.push(new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Error));
+                }
             }
         }
 
@@ -65,6 +86,12 @@ function runSemanticAnalyzer(
         if (error) {
             console.error("Error executing the analyzer:", error);
         }
+
+        fs.unlink(tmpFile, (err) => {
+            if (err) {
+                console.warn("Couldn't delete the temporal file:", tmpFile);
+            }
+        });
     });
 }
 
