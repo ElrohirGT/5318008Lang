ad567254c63b68fd52282be101590d05b844eb08
ElrohirGT <elrohirgt@gmail.com>
2025-09-02 08:37:27 -0600

    feat: Added colored output for tests!

--- main_test.go
+++ main_test.go
@@ -61,7 +61,7 @@ func Test_SnapshotTesting(t *testing.T) {
 
 		parts := strings.Split(string(fileBytes), OUTPUT_SEPARATOR)
 		cpsContents := strings.TrimSpace(parts[0])
-		programOutput := strings.TrimSpace(parts[1])
+		expectedOutput := strings.TrimSpace(parts[1])
 
 		reader := bytes.NewReader([]byte(cpsContents))
 		err = testableMain(reader)
@@ -69,24 +69,43 @@ func Test_SnapshotTesting(t *testing.T) {
 		if err != nil {
 			errMsg = strings.TrimSpace(stripANSI(err.Error()))
 		}
-		if err != nil && strings.TrimSpace(programOutput) != errMsg {
 
-			// pos, ra, rb, _ := diffChar(programOutput, errMsg)
-			// fmt.Printf("CHAR CIFF AT %d, %q vs %q \n%s\n%s\n", pos, ra, rb)
+		if err != nil && expectedOutput != errMsg {
+			b := strings.Builder{}
+			b.WriteString("\nProgram ")
+			b.WriteString(path)
+			b.WriteString(" failed with:\n")
 
-			t.Errorf(
-				"\nProgram %s failed with:\n%s\nBut expected:\n%s",
-				path,
-				errMsg,
-				programOutput,
-			)
+			lastI := 0
+			for i, expectedByte := range []byte(expectedOutput) {
+				actualByte := errMsg[i]
+				if actualByte != expectedByte {
+					b.WriteString(Red)
+				}
+				b.WriteByte(actualByte)
+				b.WriteString(Reset)
+				lastI = i
+			}
+
+			if lastI+1 < len(errMsg)-1 {
+				b.WriteString(Red)
+				b.WriteString(errMsg[lastI+1:])
+				b.WriteString(Reset)
+			}
+
+			b.WriteString("\nBut expected:\n")
+			b.WriteString(expectedOutput)
+
+			t.Error(b.String())
 			continue
 		}
-		if err == nil && programOutput != "" {
+		if err == nil && expectedOutput != "" {
 			t.Errorf(
-				"\nProgram %s didn't fail!\nBut expected:\n%s",
+				"\n%sProgram %s didn't fail!%s\nBut expected:\n%s",
+				Red,
 				path,
-				programOutput,
+				Reset,
+				expectedOutput,
 			)
 			continue
 		}
@@ -98,24 +117,3 @@ var ansiRegex = regexp.MustCompile(`\x1b\[[0-9;]*m`)
 func stripANSI(s string) string {
 	return ansiRegex.ReplaceAllString(s, "")
 }
-
-// diffChar returns the first differing rune and its position.
-// If the strings are identical, ok == false.
-// func diffChar(a, b string) (pos int, ra, rb rune, ok bool) {
-// 	ar, br := []rune(a), []rune(b)
-// 	n := min(len(br), len(ar))
-// 	for i := range n {
-// 		if ar[i] != br[i] {
-// 			return i, ar[i], br[i], true
-// 		}
-// 	}
-// 	// If all runes matched but lengths differ,
-// 	// report the first extra rune.
-// 	if len(ar) != len(br) {
-// 		if len(ar) > len(br) {
-// 			return n, ar[n], -1, true // -1 means "missing"
-// 		}
-// 		return n, -1, br[n], true
-// 	}
-// 	return 0, 0, 0, false
-// }
