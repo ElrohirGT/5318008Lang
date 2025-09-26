a78414cbf8877e391092ee19a7fdc8dc92dbc763
ElrohirGT <elrohirgt@gmail.com>
2025-09-26 15:46:38 -0600

    feat: Added files

---
+++ tac_generator/assignments.go
@@ -0,0 +1,16 @@
+package tac_generator
+
+import (
+	"github.com/ElrohirGT/5318008Lang/lib"
+	p "github.com/ElrohirGT/5318008Lang/parser"
+)
+
+func (l Listener) ExitAssignment(ctx *p.AssignmentContext) {
+	// currentScope := l.TypeChecker.ScopeManager.CurrentScope
+	l.AppendInstruction(Instruction{
+		Assignment: lib.NewOpValue(AssignmentInstruction{
+			Target: l.Program.GetNextVariableName(),
+			Type:   "i32",
+		}),
+	})
+}
