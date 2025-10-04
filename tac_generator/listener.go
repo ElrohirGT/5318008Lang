9ac0035ccb2a20466a82865dd56f22105a1283e2
ElrohirGT <elrohirgt@gmail.com>
2025-10-04 14:02:13 -0600

    fix: linter errors

--- tac_generator/listener.go
+++ tac_generator/listener.go
@@ -42,7 +42,7 @@ func (l *Listener) Generate(buff *bytes.Buffer) error {
 		log.Panicf("SKILL ISSUE:\nSomeone forgot to include the main scope inside the program!")
 	}
 
-	_, err := buff.WriteString(fmt.Sprintf("%s:\n", l.Program.MainScope))
+	_, err := fmt.Fprintf(buff, "%s:\n", l.Program.MainScope)
 	if err != nil {
 		return err
 	}
@@ -61,7 +61,7 @@ func (l *Listener) Generate(buff *bytes.Buffer) error {
 			continue
 		}
 
-		_, err := buff.WriteString(fmt.Sprintf("%s:\n", scopeName))
+		_, err := fmt.Fprintf(buff, "%s:\n", scopeName)
 		if err != nil {
 			return err
 		}
@@ -87,13 +87,13 @@ func instructionToBuffer(inst *Instruction, buff *bytes.Buffer) error {
 	switch {
 	case inst.Assignment.HasValue():
 		assignment := inst.Assignment.GetValue()
-		_, err = buff.WriteString(fmt.Sprintf("= %s %s %s",
-			assignment.Target, assignment.Type, assignment.Value))
+		_, err = fmt.Fprintf(buff, "= %s %s %s",
+			assignment.Target, assignment.Type, assignment.Value)
 
 	case inst.Copy.HasValue():
 		copy := inst.Copy.GetValue()
-		_, err = buff.WriteString(fmt.Sprintf("CP %s %s",
-			copy.Target, copy.Source))
+		_, err = fmt.Fprintf(buff, "CP %s %s",
+			copy.Target, copy.Source)
 
 	case inst.Jump.HasValue():
 		jump := inst.Jump.GetValue()
@@ -103,74 +103,74 @@ func instructionToBuffer(inst *Instruction, buff *bytes.Buffer) error {
 
 			if cond.Simple.HasValue() {
 				// Conditional simple jump
-				_, err = buff.WriteString(fmt.Sprintf("IF %s GOTO %s",
-					cond.Simple.GetValue(), jump.Target))
+				_, err = fmt.Fprintf(buff, "IF %s GOTO %s",
+					cond.Simple.GetValue(), jump.Target)
 			} else if cond.SimpleNegated.HasValue() {
 				// Conditional negation jump
-				_, err = buff.WriteString(fmt.Sprintf("IF NOT %s GOTO %s",
-					cond.SimpleNegated.GetValue(), jump.Target))
+				_, err = fmt.Fprintf(buff, "IF NOT %s GOTO %s",
+					cond.SimpleNegated.GetValue(), jump.Target)
 			} else if cond.Relation.HasValue() {
 				// Conditional multivariable jump
 				rel := cond.Relation.GetValue()
-				_, err = buff.WriteString(fmt.Sprintf("IF %s %s %s GOTO %s",
-					rel.Type, rel.P1, rel.P2, jump.Target))
+				_, err = fmt.Fprintf(buff, "IF %s %s %s GOTO %s",
+					rel.Type, rel.P1, rel.P2, jump.Target)
 			}
 		} else {
 			// Unconditional jump
-			_, err = buff.WriteString(fmt.Sprintf("GOTO %s",
-				jump.Target))
+			_, err = fmt.Fprintf(buff, "GOTO %s",
+				jump.Target)
 		}
 
 	case inst.Param.HasValue():
 		param := inst.Param.GetValue()
-		_, err = buff.WriteString(fmt.Sprintf("PARAM %s",
-			param.Parameter))
+		_, err = fmt.Fprintf(buff, "PARAM %s",
+			param.Parameter)
 
 	case inst.Call.HasValue():
 		call := inst.Call.GetValue()
 		if call.SaveReturnOn.HasValue() {
 			// call function with return
-			_, err = buff.WriteString(fmt.Sprintf("CALLRET %s %s %d",
-				call.SaveReturnOn.GetValue(), call.ProcedureName, call.NumberOfParams))
+			_, err = fmt.Fprintf(buff, "CALLRET %s %s %d",
+				call.SaveReturnOn.GetValue(), call.ProcedureName, call.NumberOfParams)
 		} else {
-			_, err = buff.WriteString(fmt.Sprintf("CALL %s %d",
-				call.ProcedureName, call.NumberOfParams))
+			_, err = fmt.Fprintf(buff, "CALL %s %d",
+				call.ProcedureName, call.NumberOfParams)
 		}
 
 	case inst.Return.HasValue():
 		ret := inst.Return.GetValue()
-		_, err = buff.WriteString(fmt.Sprintf("RETURN %s",
-			ret.Value))
+		_, err = fmt.Fprintf(buff, "RETURN %s",
+			ret.Value)
 
 	case inst.Alloc.HasValue():
 		alloc := inst.Alloc.GetValue()
-		_, err = buff.WriteString(fmt.Sprintf("ALLOC %s %d",
-			alloc.Target, alloc.Size))
+		_, err = fmt.Fprintf(buff, "ALLOC %s %d",
+			alloc.Target, alloc.Size)
 
 	case inst.LoadWithOffset.HasValue():
 		lwo := inst.LoadWithOffset.GetValue()
-		_, err = buff.WriteString(fmt.Sprintf("LWO %s %s %d",
-			lwo.Target, lwo.Source, lwo.Offset))
+		_, err = fmt.Fprintf(buff, "LWO %s %s %d",
+			lwo.Target, lwo.Source, lwo.Offset)
 
 	case inst.SetWithOffset.HasValue():
 		swo := inst.SetWithOffset.GetValue()
-		_, err = buff.WriteString(fmt.Sprintf("SWO %s %d %s",
-			swo.Target, swo.Offset, swo.Value))
+		_, err = fmt.Fprintf(buff, "SWO %s %d %s",
+			swo.Target, swo.Offset, swo.Value)
 
 	case inst.Free.HasValue():
 		free := inst.Free.GetValue()
-		_, err = buff.WriteString(fmt.Sprintf("FREE %s",
-			free))
+		_, err = fmt.Fprintf(buff, "FREE %s",
+			free)
 
 	case inst.Reference.HasValue():
 		ref := inst.Reference.GetValue()
-		_, err = buff.WriteString(fmt.Sprintf("&%s",
-			ref.Target))
+		_, err = fmt.Fprintf(buff, "&%s",
+			ref.Target)
 
 	case inst.Dereference.HasValue():
 		def := inst.Dereference.GetValue()
-		_, err = buff.WriteString(fmt.Sprintf("@%s",
-			def.Target))
+		_, err = fmt.Fprintf(buff, "@%s",
+			def.Target)
 
 	case inst.Arithmethic.HasValue():
 		// FIXME: implement me
